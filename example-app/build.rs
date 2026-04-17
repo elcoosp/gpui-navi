41aa8ca84d361a10b7bc6b91c4f2893a8fa65fdd
elcoosp <elcoosp@gmail.com>
2026-04-17 11:53:07 +0200

    feat(navigator): add preload, fix active link styling, implement can_go_back

--- example-app/build.rs
+++ example-app/build.rs
@@ -1,6 +1,7 @@
+// example-app/build.rs
 fn main() {
-    // // Generate route tree from src/routes/
-    // let config = navi_codegen::NaviConfig::from_file("navi.config.json")
-    //     .expect("Failed to read navi.config.json");
-    // navi_codegen::generator::write_route_tree(&config).expect("Failed to generate route tree");
+    println!("cargo:rerun-if-changed=src/routes");
+    let config = navi_codegen::NaviConfig::from_file("navi.config.json")
+        .expect("Failed to read navi.config.json");
+    navi_codegen::generator::write_route_tree(&config).expect("Failed to generate route tree");
 }
